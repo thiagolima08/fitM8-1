b7a8beb272593545692703c85ebf2ba872675449
Gabriel Borsero <gabriel.borsero@gmail.com>
2019-08-03 16:37:28 -0300

    cards display and button

--- js/test-exercises.js
+++ js/test-exercises.js
@@ -12,9 +12,9 @@ fetch('https://wger.de/api/v2/exerciseimage/?format=json')
     for (let i of data['results']) {
         deckTreinosDOM.insertAdjacentHTML('beforeend', `                
             <div class="card shadow card-treino">
-                    <img src="${i['image']}" class="w-50 mt-3 card-img-top img-fluid" alt="...">
+                    <img src="${i['image']}" class="mx-auto my-3 d-block card-img-top img-fluid" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Supino Plano</h5>
+                    <h5 class="card-title"><strong>Supino Plano</strong></h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <p class="card-text"></p>
                 </div>
