eebbe1179bd471b0bbcea408d72ac24c0ece275f
ifpb <ifpb@MDK27LB02-JP.ad.ifpb.edu.br>
2019-08-09 16:41:22 -0300

    Alteraçao na pagina Login e Firebase - autenticaçao completa

---
+++ js/fb.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAT89AztIqj75jCKiBkbyQa2KCZuROfHUo",
+    authDomain: "js-final-2e7f0.firebaseapp.com",
+    databaseURL: "https://js-final-2e7f0.firebaseio.com",
+    projectId: "js-final-2e7f0",
+    storageBucket: "js-final-2e7f0.appspot.com",
+    messagingSenderId: "618233221570",
+    appId: "1:618233221570:web:9dd243fafe7e36e5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
